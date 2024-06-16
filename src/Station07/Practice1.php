2a5eed1d6d3b1dd854bbd1e8b4d7e0114e2ade56
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-16 22:53:30 +0900

    railway7 clear

--- src/Station07/Practice1.php
+++ src/Station07/Practice1.php
@@ -6,7 +6,17 @@ class Practice1
 {
     public function main(): void
     {
-        // ここにサンプルコードを記述
+        $array1 = [4, 5];
+        $array2 = [1, 2, 3];
+
+        $mergedArray = array_merge($array1, $array2);
+        print_r($mergedArray);
+
+        $array3 = ['apple' => 'red', 'banana' => 'yellow', 'rime' => 'green'];
+        $array4 = ['strawberry' => 'pink', 'banana' => 'yellow'];
+
+        $mergedArray2 = array_merge($array3, $array4);
+        print_r($mergedArray2);
     }
 }
 
--- src/Station07/Question.php
+++ src/Station07/Question.php
@@ -9,12 +9,21 @@ class Question
         $array1 = ['北海道' => 1, '東京都' => 13, '大阪府' => 'XX'];
         $array2 = ['広島県' => 34, '京都府' => 26];
         $array3 = ['京都府' => 'XX', '大阪府' => 27];
+
+        $mergedArray = array_merge($array1, $array3, $array2);
+        return $mergedArray;
     }
 
     public function two(): array
     {
         $firstNames = ['太郎', '次郎', '花子'];
         $lastNames = ['山田', '鈴木', '佐藤'];
+
+        $mergedArray2 = array_map(function ($key) use ($lastNames, $firstNames) {
+            return $lastNames[$key] .  $firstNames[$key];
+        }, array_keys($lastNames));
+
+        return $mergedArray2;
     }
 
     public function three(): array
@@ -24,5 +33,48 @@ class Question
             ['name' => 'banana', 'price' => 200],
             ['name' => 'orange', 'price' => 120],
         ];
+
+        $array4 = array_column($array, 'price','name');
+        return $array4;
     }
 }
+
+// (new Question())->three();
+
+// Question.php ファイルの one メソッド内に記述してください。
+
+// 定義済みの 3 つの配列を、以下の 1 つの配列になるよう結合して、返り値にしてください。
+
+// ただし、要素の並び順は以下の通りでなくても構いません。
+
+
+// [
+//     '北海道' => 1,
+//     '東京都' => 13,
+//     '京都府' => 26,
+//     '大阪府' => 27,
+//     '広島県' => 34,
+// ]
+// 問題2
+// Question.php ファイルの two メソッド内に記述してください。
+
+// 定義済みの 2 つの配列について、名字と名前を結合した下記の配列を作成して、返り値にしてください。
+
+
+// ['山田太郎', '鈴木次郎', '佐藤花子']
+// 問題3
+// Question.php ファイルの three メソッド内に記述してください。
+
+// 定義済みの配列について、以下の連想配列になるようカラムを抽出して、返り値にしてください。
+
+
+// [
+//     'apple' => 140,
+//     'banana' => 200,
+//     'orange' => 120,
+// ]
+
+// クリア条件
+// 期待値と一致するよう結合された連想配列を返す
+// 結合した姓名を要素に持つ配列を返す
+// 期待値と一致するよう抽出された連想配列を返す
