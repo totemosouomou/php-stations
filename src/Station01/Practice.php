80de8fe81a347a709ee78468482cd1076043403f
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-16 13:57:09 +0900

    railway1 clear

--- src/Station01/Practice.php
+++ src/Station01/Practice.php
@@ -6,7 +6,8 @@ class Practice
 {
     public function main(): void
     {
-        // ここにサンプルコードを記述
+        $a = 1;
+        echo gettype($a) . PHP_EOL;
     }
 }
 
--- src/Station01/Question.php
+++ src/Station01/Question.php
@@ -6,9 +6,14 @@ class Question
 {
     public function main(): array
     {
-        $a = ;
-        $b = ;
+        $a = 1;
+        echo gettype($a) . PHP_EOL;
+
+        $b = true;
+        echo gettype($b) . PHP_EOL;
 
         return [$a, $b];
     }
 }
+
+(new Question())->main();
