617c839bbcc219a2f4c5f7a8532bfac8ee8f3dfd
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-17 07:22:27 +0900

    railway8 clear

--- src/Station08/Practice.php
+++ src/Station08/Practice.php
@@ -6,7 +6,14 @@ class Practice
 {
     public function main(): void
     {
-        // ここにサンプルコードを記述
+        $array = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        array_push($array, [10, 11, 12], [13, 14]);
+        print_r($array);
     }
 }
 
