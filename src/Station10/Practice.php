84e77d7e3c30cbd6b5ff208d07c1b177e8dcd4e5
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-17 13:57:05 +0900

    railway10 clear

--- src/Station10/Practice.php
+++ src/Station10/Practice.php
@@ -6,6 +6,16 @@ class Practice
 {
     public function main(): void
     {
+        $this->checkEvenOrOdd(11);
+    }
+
+    private function checkEvenOrOdd($number)
+    {
+        if ($number % 2 === 0) {
+            echo $number.'は偶数です';
+        } else {
+            echo $number.'は奇数です';
+        }
     }
 }
 
--- src/Station10/Question.php
+++ src/Station10/Question.php
@@ -11,5 +11,30 @@ class Question
 
     private function getAnimalName(string $animal): string
     {
+        if($animal === '猫') {
+            return 'ミケ';
+        }
+        if($animal === '犬') {
+            return 'ポチ';
+        }
+        return 'わかりません';
     }
 }
+
+// echo (new Question())->main('アヒル');
+
+// src/Station10/Question.php ファイルの Question クラス内に記述してください。
+
+// 引数 $animalの値に応じて、返り値が以下の通りとなるメソッド getAnimalName を作成してください。
+
+// また、作成したメソッドは main メソッド内で呼び出してください。
+
+// $animal の値	返り値
+// 猫	ミケ
+// 犬	ポチ
+// 上記以外	わかりません
+// クリア条件
+// 1
+// 引数に応じて動物の名前を返す
+// 2
+// 仕様に準じたgetAnimalNameメソッドが定義されている
