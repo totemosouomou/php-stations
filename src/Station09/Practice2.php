812c8f52f64b8776cbd5f42f1b8acf82ee36365d
totemosouomou <totemosouomou@users.noreply.github.com>
2024-06-17 09:07:06 +0900

    railway9 clear

--- src/Station09/Practice2.php
+++ src/Station09/Practice2.php
@@ -6,7 +6,25 @@ class Practice2
 {
     public function main(): void
     {
-        // ここにサンプルコードを記述
+        $fruits = [
+            'apple' => 'red',
+            'banana' => 'yellow',
+            'rime' => 'green',
+            'strawberry' => 'red',
+            'pineapple' => 'yellow',
+        ];
+
+        $result = [];
+
+        foreach ($fruits as $key => $fruit) {
+            if ($fruit === 'yellow') {
+                continue;
+            }
+
+            $result[$key] = $fruit;
+        }
+
+        print_r($result);
     }
 }
 
--- src/Station09/Question.php
+++ src/Station09/Question.php
@@ -6,5 +6,52 @@ class Question
 {
     public function main(): array
     {
+        $users = [
+            [
+                'id' => 1,
+                'last_name' => '山田',
+                'first_name' => '太郎',
+                'age' => 20,
+                'password' => 'yamada',
+            ],
+            [
+                'id' => 2,
+                'last_name' => '鈴木',
+                'first_name' => '次郎',
+                'password' => 'suzuki',
+            ],
+            [
+                'id' => 3,
+                'last_name' => '佐藤',
+                'first_name' => '花子',
+                'password' => 'sato',
+            ],
+        ];
+
+        $array = [];
+        foreach ($users as $user) {
+            $filteredUser = [];
+            foreach ($user as $key => $value) {
+                if ($key !== 'password') {
+                    $filteredUser[$key] = $value;
+                }
+                $filteredUser['full_name'] = $user['last_name'] . $user['first_name'];
+
+                if (empty($filteredUser['age'])) {
+                    $filteredUser['age'] = null;
+                }
+            }
+            $array[] = $filteredUser;
+        }
+        // print_r($array);
+        return $array;
     }
 }
+
+// echo (new Question())->main();
+
+// 'last_name' と 'first_name' を結合した要素を持つ、 'full_name' キーを追加（例: 'full_name' => '山田太郎' )
+// 'age' キーの存在しない要素について、 'age' => null を追加
+// 'password' キーを削除
+// 上記全ての処理を行った配列を、 main メソッドの返り値とする
+// 返り値となる配列内の要素の順序は問わない
